b92ea316f2b85c176f8aa424e963e3acc761056a
Justin Smith <justin@isamaker.com>
2018-11-28 09:46:08 -0600

    Add validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,19 +8,31 @@ const passport = require('passport');
 // Load user model
 const User = require('../../models/User');
 
+// Load validation
+const validateRegisterInput = require('../../validation/register');
+
 // @route   POST /users/register
 // @desc    Register a user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+
   // check if user already registered with this e-mail
   User.findOne({ email: req.body.email })
     .then(user => {
       if(user){
+        errors.email = 'Email already exists';
         return res.status(400).json(errors);
       } else {
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
+          username: req.body.username,
           password: req.body.password
         });
 
