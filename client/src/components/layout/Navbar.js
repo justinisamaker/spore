0a636f3d366af0ef48ab9b49c4112b59e4d352fc
= <justin@isamaker.com>
2018-12-05 13:33:00 -0600

    Start adding react front-end, get authentication for front-end working

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
+
+class Navbar extends Component{
+  onLogoutClick(e){
+    e.preventDefault();
+    this.props.clearCurrentProfile();
+    this.props.logoutUser();
+  }
+  render(){
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/" onClick={this.onLogoutClick.bind(this)}>Log Out</Link>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul>
+        <li>
+          <Link to="/register">Sign Up</Link>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    );
+
+    return(
+      <nav>
+        { isAuthenticated ? authLinks : guestLinks }
+      </nav>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
