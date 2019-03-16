4416099a45a16f2b1e60cccea96843683d55051e
Justin Smith <justin@isamaker.com>
2019-03-15 21:21:12 -0500

    Fix keys file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://sojournadmin:Ce4vC867rYDFC3yTdWXk@freesojourncluster-w08xn.mongodb.net/test?retryWrites=true',
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
