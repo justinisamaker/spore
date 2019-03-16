1b11422c2456ae61a9d74c099eb39954bece7eab
Justin Smith <justin@isamaker.com>
2019-03-15 22:07:57 -0500

    one more stab at not serving the endpoint in the react app

--- server.js
+++ server.js
@@ -76,10 +76,9 @@ app.use('/api/systemstatus', systemStatus);
 
 // Serve static assets if in prod
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static('client/build')); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
 
