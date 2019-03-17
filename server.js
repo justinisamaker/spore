50260bf308404778bc0d83623efca81045a8858f
Justin Smith <justin@isamaker.com>
2019-03-17 17:28:29 -0500

    Server static assets if in prod

--- server.js
+++ server.js
@@ -69,8 +69,10 @@ app.use('/api/systemstatus', systemStatus);
 
 // Serve static assets if in prod
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build')); // serve the static react app
-}
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // Define global variables
 global.globalHumidity = null;
