05191ac3fc4bc648b637f47612f72e31f5524360
Justin Smith <justin@isamaker.com>
2019-03-17 16:38:40 -0500

    Trying other server changes

--- server.js
+++ server.js
@@ -211,5 +211,5 @@ cron.schedule('1 * * * * *', () => {
 });
 
 // Start server listening
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
