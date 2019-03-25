9f3d62d806d2711f693729fa58cb3193cbc13361
Justin Smith <justin@isamaker.com>
2019-03-25 12:46:30 -0500

    Make setpoint buttons disabled if not logged in

--- client/src/components/currentreadings/CurrentReading.js
+++ client/src/components/currentreadings/CurrentReading.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './CurrentReading.scss';
 import { Line } from 'rc-progress';
 import { Button } from '../button/Button';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class CurrentReading extends Component{
   constructor(props){
@@ -50,7 +52,18 @@ class CurrentReading extends Component{
     axios.post(`/api/${this.props.modifier}/setpoint/${this.state.setpoint}`);
   }
 
+
   render(){
+    let buttonsDisabled;
+    const AuthCheck = () => {
+      const { isAuthenticated } = this.props.auth;
+      if(!isAuthenticated){
+        buttonsDisabled = true;
+      } else {
+        buttonsDisabled = false;
+      }
+      return buttonsDisabled;
+    }
     const tempFormatting = (`${this.state.readingvalue}ºF`);
     const humidityFormatting = (`${this.state.readingvalue}%`);
     const tempSetpointFormatting = (`${this.state.setpoint}ºF`);
@@ -58,13 +71,30 @@ class CurrentReading extends Component{
     const tempLineColor = '#fd8524';
     const humidityLineColor = '#3D99FE';
 
+    const IncrementButton = () => {
+      if(!buttonsDisabled){
+        return(<Button text="+" onClick={() => this.changeSetpoint('increment')} size="large" />);
+      } else {
+        return(<Button text="+" onClick={() => this.changeSetpoint('increment')} size="large" disabled="disabled" />);
+      }
+    }
+
+    const DecrementButton = () => {
+      if(!buttonsDisabled){
+        return(<Button text="-" onClick={() => this.changeSetpoint('decrement')} size="large" />);
+      } else {
+        return(<Button text="-" onClick={() => this.changeSetpoint('decrement')} size="large" disabled="disabled" />);
+      }
+    }
+
     return(
       <div className="current-reading">
+        <AuthCheck />
         <h4 className="current-reading-title">{this.props.modifier}</h4>
         <Line percent={this.state.readingvalue} strokeWidth="2" strokeColor={this.props.modifier === 'Temperature' ? tempLineColor : humidityLineColor} />
         <div className="current-reading-value">
           <div className="increment-button setpoint-button">
-            <Button text="+" onClick={() => this.changeSetpoint('increment')} size="large"/>
+            <IncrementButton />
           </div>
           <div className="reading-value value">
             <h5>{this.props.modifier === 'Temperature' ? tempFormatting : humidityFormatting}</h5>
@@ -75,7 +105,7 @@ class CurrentReading extends Component{
             <h6>Setpoint</h6>
           </div>
           <div className="decrement-button setpoint-button">
-            <Button text="-" onClick={() => this.changeSetpoint('decrement')} size="large"/>
+            <DecrementButton />
           </div>
         </div>
       </div>
@@ -83,4 +113,12 @@ class CurrentReading extends Component{
   }
 }
 
-export default CurrentReading;
+CurrentReading.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { })(CurrentReading);
